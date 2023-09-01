fc89d92063cfe51bf1217a35b63315c6ec6249b0
Vanessa <126210620+nessarz93@users.noreply.github.com>
2023-09-01 23:15:02 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let quien = who[Math.floor(Math.random() * 4)];
+  let accion = action[Math.floor(Math.random() * 4)];
+  let que = what[Math.floor(Math.random() * 4)];
+  let donde = when[Math.floor(Math.random() * 4)];
+
+  let result = quien + " " + accion + " " + que + " " + donde;
+
+  let excuseElement = document.getElementById("result");
+  excuseElement.textContent = result;
+
+  console.log(result);
 };
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
